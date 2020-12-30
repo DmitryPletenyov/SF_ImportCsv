33af5826a7ee67689effe64c62c86350c55544ca
DmitryPletenyov <dpletenyov@gmail.com>
2020-12-30 17:35:16 +0100

    Get ArtikulId By CatalogIndex

--- test.php
+++ test.php
@@ -5,18 +5,14 @@ include "vendor/autoload.php";
 use Goutte\Client;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\DomCrawler\Crawler;
+use Phppot\DataSource;
+
+require_once 'DataSource.php';
 
 $client = HttpClient::create();
 
-$catalogIndex = 'DTK.18.045.16.0SR';
-//$url = 'https://b2b-itatools.pl/ProduktySzczegoly.aspx?id_artykulu=frYtxkWtdj7OVYk5F1v-4w';
-$url = 'https://b2b-itatools.pl/ProduktyWyszukiwanie.aspx?search='.@catalogIndex;
-
-$url = 'https://b2b-itatools.pl/ProduktyWyszukiwanie.aspx?mikat=-2147483648&search=DTK.18.045.16.0SR';
-
-$cookieSearch = 'mistral=md5=5CF8AF96B465FC3C85E4A9B2718A203B; _ga=GA1.2.1362453477.1607516709; czater__first-referer=https://b2b-itatools.pl/Default.B2B.aspx; czater__63d2198880f9ca34993a3cc417bc1912fd5fb897=eae29a7bfd11b99d10de1c243836d880; ASP.NET_SessionId=0210mkeidqvj3xg5ka1ss3jh; _gid=GA1.2.1883084225.1608639277; czater__open2_63d2198880f9ca34993a3cc417bc1912fd5fb897=0; czater__teaser_shown=1608639283858; _gat=1';
-
-$response = $client->request('GET',     $url,
+function executeGet(string $url, string $cookie, object $client) {
+	$response = $client->request('GET',     $url,
     ['headers' => [
 		'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0',
         'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
@@ -24,70 +20,129 @@ $response = $client->request('GET',     $url,
 		'Host' =>	'b2b-itatools.pl',
 		'Upgrade-Insecure-Requests' => '1',
 		'Connection' => 'keep-alive',
-		//'Content-Type' => 'multipart/form-data; boundary=---------------------------35364124307512379322088837747',
-		//'Content-Length' => '2295',
 		'Origin' => 'https://b2b-itatools.pl',
 		'Referer' => $url,
 		'TE' => 'Trailers',
-		'Cookie' => $cookieSearch]]);
-
-
-
-$statusCode = $response->getStatusCode();
-
-echo $statusCode;
-
-if ($statusCode == 302) {
-	echo 'yes!!';
+		'Cookie' => $cookie]]);
+		
+  return $response;
 }
 
-if ($statusCode == 200) {
-	$content = $response->getContent();
-	$crawler = new Crawler($content);
-	
-	if ($crawler->filter('input#ctl00_MainContent_tbLogin')->count() > 0) {
-		echo 'Need to log in. Possible Expired cookies.';
-	}
-	var_dump($crawler->html());
-	
-	$kategory = $crawler->filter('body')->first();
-	
-	//var_dump($kategory->html());
-	
-	// ---- Net price (including discounts) ---- 
-	//$price1html = $crawler->filter('div#szczegolyProduktu p.cena_netto')->first()->html('Missing price1', false);
-	
-	//$price1 = substr($price1html, 0, strpos($price1html, '<'));
-	//var_dump($price1);
-	
-	// ---- Items' count  ---- 
-	//$cnt = $crawler->filter('div#daneDodatkowe p')->eq(2)->text();
 
-	// ---- Net price (before discount) ---
-	$price2 = '';
-	$price2table = $crawler->filter('table#tabelaInfoDodatkowe tr');
-	foreach ($price2table as $domElement) {
-		if (str_starts_with($domElement->nodeValue, 'Net price (before discount):')) {
-			// get number from "Net price (before discount):175,00 EUR"
-			$start = strpos($domElement->nodeValue, ':');
-			$price2 = substr($domElement->nodeValue, $start+1, (strpos($domElement->nodeValue, ' ', $start)-$start-1));
-			//var_dump($price2);
+
+function getArtikulIdByCatalogIndex(string $catalogIndex, string $cookieSearch, object $client) {
+	$artikulId = "";
+	
+	$url = 'https://b2b-itatools.pl/ProduktyWyszukiwanie.aspx?mikat=-2147483648&search='.$catalogIndex;
+
+	$response = executeGet($url, $cookieSearch, $client);
+
+	$statusCode = $response->getStatusCode();
+
+
+	if ($statusCode == 200) {
+		$content = $response->getContent();
+		$crawler = new Crawler($content);
+		
+		if ($crawler->filter('input#ctl00_MainContent_tbLogin')->count() > 0) {
+			echo 'Need to log in. Possible Expired cookies.'.'<br>';
+		}
+		//var_dump($crawler->html());
+		
+		// href="javascript:__doPostBack('ctl00$MainContent$miMistralKategorie','kategoria_32362')"
+		$foundItem = $crawler->filter('div.kafelek');
+		
+		// 26 found items means nothing was found
+		if ($foundItem->count() > 0 && $foundItem->count() < 26) {
+			// something was found
+			// try to get category id 
+			$href = $foundItem->first()->filter('a')->first()->attr('href');
+			
+			if (strlen($href) > 0) {
+				$start = strpos($href, 'kategoria_');
+				$href = substr($href, $start);
+				$end = strpos($href, "'");
+				$catId = substr($href, 10, $end - 10);
+				
+				$url = 'https://b2b-itatools.pl/ProduktyWyszukiwanie.aspx?search=&mikat='.$catId;
+				$response = executeGet($url, $cookieSearch, $client);
+				$statusCode = $response->getStatusCode();
+				if ($statusCode == 200) {
+					$content = $response->getContent();
+					$crawler = new Crawler($content);
+					
+					$crawler = $crawler->filter('tbody.tbxRows tr');
+					
+					// find row in result table
+					$rowNumber = -1;
+					$rowWasFound = false;
+					foreach ($crawler as $domElement) {
+						$rowNumber++;
+						if (str_contains ($domElement->nodeValue, 'Catalogue index:'.$catalogIndex)) {
+							$rowWasFound = true;
+							$href = $crawler->eq($rowNumber)->filter('td')->eq(1)->filter('div a')->first()->attr('href');
+							
+							//ProduktySzczegoly.aspx?id_artykulu=rBv72rbPPyDT3sAt-rxLl
+							$start = strpos($href, 'id_artykulu=');
+							$artikulId = substr($href, $start + 12);
+							//echo ' '.$catalogIndex.' = '.$artikulId;
+							break;
+						}
+					}
+					
+					if ($rowWasFound == false) {
+						echo "Row for ".$catalogIndex." wasn't wound".'<br>';
+					}
+				}else {
+					echo 'Status code ='.$statusCode.' for '.$url.'<br>';
+				}
+			}
+		} else {
+			//echo 'Nothing was found'.'<br>';
+		}	
+	} else {
+		echo 'Status code ='.$statusCode.' for '.$url.'<br>';
+	}
+	
+	return $artikulId;
+}
+
+$cookieSearch = 'ASP.NET_SessionId=raxb2yanq15ug1is5etvjwsk; mistral=md5=976C3B5336B4EC1F8207F9F0487BE3B6; _ga=GA1.2.1386337413.1609321364; _gid=GA1.2.2084395790.1609321364; czater__first-referer=https://b2b-itatools.pl/Default.B2B.aspx; czater__63d2198880f9ca34993a3cc417bc1912fd5fb897=c02edda4a204966c53f5f779d51b0bae; czater__open2_63d2198880f9ca34993a3cc417bc1912fd5fb897=0; czater__teaser_shown=1609321500298; _gat=1';
+
+$db = new DataSource();
+$conn = $db->getConnection();
+
+$top10Rows = "Select id, productnumber from products LIMIT 3";
+$result = $db->select($top10Rows);
+if (! empty($result)) {
+	foreach ($result as $row) {		
+		$catalogIndex = $row['productnumber'];
+		//echo "======= Start ".$catalogIndex." =======";
+		$artikulId = getArtikulIdByCatalogIndex($catalogIndex, $cookieSearch, $client);
+		
+		if ($artikulId != '') {
+			echo $catalogIndex.' = '.$artikulId.'<br>';
+		} else {
+			echo $catalogIndex.' - '."doesn't exist in ITA".'<br>';
 		}
 		
+		//echo "======= Finish ".$catalogIndex." =======";
 	}
-	
-	//echo 'Count: '.$cnt.'<br/>';
-	//echo 'Net price (including discounts): '.$price1.'<br/>';
-	//echo 'Net price (before discount): '.$price2.'<br/>';
-	
 }
 
+// Limit of 120 sec. It takes 10 sec to process each record. 
+// maybe add query string parameter (1-9, 10-19, 20-29, ...) and run in parallel? DB lock?
+// Add db table with ITA internal id (artikulId)
 
-
-
-
-
-
+			
+/*
+$catalogIndex = 'DTK.18.045.16.0SR';
+$artikulId = getArtikulIdByCatalogIndex($catalogIndex, $cookieSearch, $client);
+$catalogIndex = 'EVLC Jig1';
+$artikulId = getArtikulIdByCatalogIndex($catalogIndex, $cookieSearch, $client);
+$catalogIndex = '193.12.032.090.12Ra';
+$artikulId = getArtikulIdByCatalogIndex($catalogIndex, $cookieSearch, $client);
+*/
 
 
 
